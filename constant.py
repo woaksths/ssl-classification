ae8bd9f74051beef38855c14869aca622b71c454
jaeman <woaksths@gmail.com>
2021-01-05 22:19:40 +0900

    add necessary config file

--- constant.py
+++ constant.py
@@ -1,6 +1,8 @@
 import torch
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 learning_rate = 2e-5
-epochs = 4
-train_batch_size = 4
-valid_batch_size = 4
+epochs = 30
+train_batch_size = 16
+valid_batch_size = 16
+
+
