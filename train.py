117c45d8aa24f6fc5b8d12e58cd82f3020431fa9
jaeman <woaksths@gmail.com>
2021-01-08 23:10:42 +0900

    add pseudo-labeling in SSL module

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from dataset.sampling import sample_dataset, gen_perturbed_samples
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer
 from trainer.trainer import Trainer
+from trainer.ssl_trainer import SSL_Trainer
 from augment import text_augment
 import constant as config
 from lexicon_util.lexicon_operation import Lexicon
@@ -27,7 +28,7 @@ test_texts, test_labels = read_imdb_split(args.test_path)
 
 # sample dataset
 labeled_data, unlabeled_data, dev_data = sample_dataset(dataset=(train_texts,train_labels), sampling_ratio= [0.5, 0.5],
-                                                        is_balanced=True, class_num=2, sampling_num=60)
+                                                        is_balanced=True, class_num=2, sampling_num=200)
 
 sampled_text, sampled_label = labeled_data[0], labeled_data[1]
 unlabeled_text, unlabeled_label = unlabeled_data[0], unlabeled_data[1]
@@ -48,31 +49,21 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 train_encodings = tokenizer(sampled_text, truncation=True, padding=True)
 val_encodings = tokenizer(dev_text, truncation=True, padding=True)
-test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+# test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+# unlabeled_encodings = tokenizer(unlabeled_text, truncation=True, padding=True)
 
 train_dataset = ImdbDataset(train_encodings, sampled_label)
 val_dataset = ImdbDataset(val_encodings, dev_label)
-test_dataset = ImdbDataset(test_encodings, test_labels)
-
-# Define hyperparameters
-train_params = {'batch_size': config.train_batch_size,
-                'shuffle':True,
-                'num_workers':0
-               }
-valid_params = {'batch_size':config.valid_batch_size,
-                'shuffle':False,
-                'num_workers':0
-               }
-test_params = {'batch_size':128,
-                'shuffle':False,
-                'num_workers':0
-               }
+# test_dataset = ImdbDataset(test_encodings, test_labels)
+# unlabeled_dataset = ImdbDataset(unlabeled_encodings, unlabeled_label)
 
 # Load dataset
-train_loader = DataLoader(train_dataset, **train_params)
-valid_loader = DataLoader(val_dataset, **valid_params)
-test_loader = DataLoader(test_dataset, **test_params)
+train_loader = DataLoader(train_dataset, **config.train_params)
+valid_loader = DataLoader(val_dataset, **config.valid_params)
+# test_loader = DataLoader(test_dataset, **cofnig.test_params)
+# unlabeled_loader = DataLoader(unlabeled_dataset, **cofnig.unlabeled_params)
 
+'''
 # Build model & Criterion
 model = BERT_LSTM_Classification(class_num=2, vocab_size=None)
 
@@ -83,34 +74,30 @@ optimizer = torch.optim.Adam(params = model.parameters(), lr=config.learning_rat
 trainer = Trainer(config=config, model=model, criterion=loss_function, optimizer = optimizer,
                   train_loader=train_loader, valid_loader=valid_loader, test_loader=test_loader, save_path= args.save_path, tokenizer=tokenizer)
 trainer.train()
-
 del model, optimizer
+'''
 
 # Initialization model and optimizer
 model = BERT_LSTM_Classification(class_num=2, vocab_size=None)
 optimizer = torch.optim.Adam(params = model.parameters(), lr=config.learning_rate)
 
+# load checkpoint
 checkpoint = torch.load('experiment/lowest_val_loss.pt')
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 epoch = checkpoint['epoch']
 
-print('epoch', epoch)
-# get initial lexicon 
-lexicon = Lexicon(epoch=epoch)
-print(lexicon.lexicon)
-lexicon.augment_lexicon()
-print(lexicon.lexicon)
+# get initial lexicon
+lexicon_instance = Lexicon(epoch=epoch)
+print(lexicon_instance.lexicon)
+lexicon_instance.augment_lexicon()
 
-# Semi supervised learning 
-'''
-###########################################################
-TODO
-1. SSL 학습 및 데이터 업데이트 모듈 구현 
-2. 렉시콘 업데이터 모듈 구현
-3. perturbed sample 생성 -> 메타러닝
-###########################################################
-''' 
-### confidence 가 낮은 데이터에 대해서는 pseudo-label이 되지 못하므로 학습이 안되는 경향이 있는데
-### 이를 위해서 반대 클래스 들에 대해서 perturbed sample을 생성 ... 
-### pseudo-label시 가장 큰 클래스 기준 over-sample
+# Semi-supervised learning
+print('SEMI-SUPERVISED LEARNING')
+print(lexicon_instance.lexicon)
+ssl_trainer = SSL_Trainer(expt_dir=args.save_path, criterion=torch.nn.CrossEntropyLoss(),
+                          lexicon_instance=lexicon_instance, config=config)
+
+ssl_trainer.train(labeled_data = train_dataset, unlabeled_data=train_dataset,
+                  dev_data=val_dataset, test_data = val_dataset, outer_epoch=20, inner_epoch=20)
+1/0
--- trainer/ssl_trainer.py
+++ trainer/ssl_trainer.py
@@ -1,11 +1,18 @@
 import torch
 import os
+from util.early_stopping import EarlyStopping
+from util.checkpoint import save_model, get_best_checkpoint, load_trained_model
+from transformers import BertTokenizer
+from model.bert_classification import BERT_Classification
+from torch.utils.data import Dataset, DataLoader
+from torch import cuda
+import torch.nn as nn 
+import copy
 
 class SSL_Trainer(object):
     
     '''
     TO DO
-    
     1. PSEUDO-LABELING 
     2. UPDATE DATASET (1. add labeled data into train data 2. remove labeled data from unlabeled data)
     3. MAKE PERTURBED SAMPLES from labeled data
@@ -16,55 +23,183 @@ class SSL_Trainer(object):
 
     7. DEV EVAL (if possible test eval)
     8. SAVING & LOAD MODULE
-   
     '''
     
-    def __init__(self, ):
-        '''
-        unlabeled_data
-        labeled_data
-        test_data (if possible)
-        tokenizer
+    def __init__(self, expt_dir=None, criterion= nn.CrossEntropyLoss(), lexicon_instance=None, config=None):
+        self.expt_dir = expt_dir
+        self.ssl_expt_dir = expt_dir +'/SSL'
+        self.config = config
+        self.device = self.config.device
+        self.criterion = criterion
+        self.lexicon_instance = lexicon_instance
+        self.tokenizer =  BertTokenizer.from_pretrained('bert-base-uncased')
+        if not os.path.isabs(self.ssl_expt_dir):
+            self.ssl_expt_dir = os.path.join(os.getcwd(), self.ssl_expt_dir)
+        if not os.path.exists(self.ssl_expt_dir):
+            os.makedirs(self.ssl_expt_dir)
         
-        model
-        optimizer
-        criterion
+
         
-        device
-        lexicon_obj
+    def update_dataset(self, new_labeled_dataset, labeled_dataset):
         '''
-        pass
+        @param new_labeled_dataset: type list, list의 element: tuple (dict:encodings, list:labels)
+        @param labeled_dataset: DATASET composed of encodings and labels
+        @return training_dataset = new_labeled_dataset + labeled_dataset
+        '''
+        training_dataset = copy.deepcopy(labeled_dataset)
+
+        for new_labeled_data in new_labeled_dataset:
+            input_ids = new_labeled_data[0]['input_ids']
+            attention_mask = new_labeled_data[0]['attention_mask']
+            token_type_ids = new_labeled_data[0]['token_type_ids']
+            labels = new_labeled_data[1]
+            
+            training_dataset.encodings['input_ids'].extend(input_ids)
+            training_dataset.encodings['attention_mask'].extend(attention_mask)
+            training_dataset.encodings['token_type_ids'].extend(token_type_ids)
+            training_dataset.labels.extend(labels)
+            
+        return training_dataset
+
+        
+    def pseudo_labeling(self, batch, model_preds, lexicon):
+        decode_sents = []
+        # decoding input_ids
+        for ids in batch['input_ids']:
+            sent = self.tokenizer.decode(ids)
+            sent = sent.replace('[CLS]','')
+            sent = sent.replace('[PAD]', '')
+            sent = sent.replace('[SEP]', '').strip()
+            decode_sents.append(sent)
+        
+        # lexicon based labeling
+        lexicon_preds = []
+        for sent in decode_sents:
+            matching_cnt = {label:0 for label in lexicon.keys()}
+            for word in sent.split(' '):
+                for label in lexicon.keys():
+                    if word in lexicon[label]:
+                        matching_cnt[label] += 1
+                        
+            lexicon_pred = -1
+            max_cnt = -1
+            for label, cnt in matching_cnt.items():
+                if max_cnt < cnt:
+                    max_cnt = cnt
+                    lexicon_pred = label
+            lexicon_preds.append(lexicon_pred)
+        
+        labeled_encodings = {'input_ids':[], 'token_type_ids':[], 'attention_mask':[]}
+        labeled_labels = []
+        
+        # lexicon & model consensus labeling
+        total, correct = 0, 0
+        for idx, (model_pred, lexicon_pred) in enumerate(zip(model_preds, lexicon_preds)):
+            model_pred = model_pred.item()
+            pred_label = -1
+            target = batch['labels'][idx].item()
+            
+            if model_pred == lexicon_pred:
+                pred_label = lexicon_pred
+                
+                labeled_encodings['input_ids'].append(batch['input_ids'][idx].tolist())
+                labeled_encodings['token_type_ids'].append(batch['token_type_ids'][idx].tolist())
+                labeled_encodings['attention_mask'].append(batch['attention_mask'][idx].tolist())
+                labeled_labels.append(pred_label)
+                total +=1
+                
+            if target == pred_label:
+                correct +=1
+                
+        return (labeled_encodings, labeled_labels), (correct, total)
     
     
+    
+    def train(self, labeled_data=None, unlabeled_data=None,
+              dev_data=None, test_data=None, outer_epoch=20, inner_epoch=20):
+        outer_early_stopping = EarlyStopping(patience=5, verbose=True)
+        
+        ## Outer Loop
+        for o_epoch in range(outer_epoch):
+            if outer_early_stopping.early_stop:
+                break
+                
+            # init model and load best model 
+            checkpoint = get_best_checkpoint(self.expt_dir, is_best_acc=False) # expt_dir -> ssl_expt_dir
+            model, optimizer = load_trained_model(checkpoint, load=True)
+            model.to(self.device)
+            model.eval()
+            unlabeled_loader = DataLoader(unlabeled_data,  **self.config.unlabeled_params)
+            with torch.no_grad():
+                # both model and lexicon pred
+                consensus_total = 0 
+                consensus_correct = 0
+                # using only model pred
+                model_total = 0
+                model_correct = 0
+                
+                new_labeled_dataset = []
+                
+                for _, batch in enumerate(unlabeled_loader):
+                    ids = batch['input_ids'].to(self.device, dtype=torch.long)
+                    attention_mask = batch['attention_mask'].to(self.device, dtype=torch.long)
+                    token_type_ids = batch['token_type_ids'].to(self.device, dtype=torch.long)
+                    targets = batch['labels'].to(self.device, dtype=torch.long)
+                    
+                    outputs, attn = model(ids, attention_mask, token_type_ids)
+                    
+                    big_val, big_idx = torch.max(outputs.data, dim=1) # -> softmax
+                    model_correct += (big_idx==targets).sum().item()
+                    model_total += targets.size(0)
+                    
+                    (labeled_encodings, labeled_labels), (correct, total) = self.pseudo_labeling(batch, big_idx, self.lexicon_instance.lexicon)
+                    new_labeled_data = (labeled_encodings, labeled_labels)
+                    consensus_correct += correct
+                    consensus_total += total
+                    new_labeled_dataset.append(new_labeled_data)
+                    
+            print('INFO correct {} / total {} by only model predict'.format(model_correct, model_total))
+            print('INFO consensus correct {} / consensus total {}'.format(consensus_correct, consensus_total))
+            
+            print('#'*100)
+            print('UPDATE DATASET')
+            print('LABELED DATA :',len(labeled_data))
+            train_dataset = self.update_dataset(new_labeled_dataset, labeled_data)
+            print('TRAINING DATA: ', len(train_dataset))
+            train_loader = DataLoader(train_dataset, **self.config.train_params)
+            
+            # Inner Loop 
+            '''
+            1. 추가된 학습 데이터를 통해 학습을 진행
+            2. 각 epoch의 배치마다 매번 perturbed samples을 다르게 생성하도록 구현 
+            3. after training all the epoch or early stopping, extract lexicon for the total labeled training dataset. 
+            4. update existing lexicon based on extracted lexicon
+            '''
+            inner_early_stopping = EarlyStopping(patience=5, verbose=True)
+            model.train()
+            for i_epoch in range(inner_epoch):
+                # load labeled dataloader
+                for _, batch in enumerate(labeled_loader):
+                    # forward batch 
+                    # get loss1 
+                    
+                    # gen_perturbed_samples per batch
+                    # get loss2
 
-    def train():
-        ## outer epoch 
-        ## inner epoch 
-        
-        # inner loop early stopping
-        # outer loop early stopping 
+                    # loss = loss1 + loss2 
 
-        ## Outer Loop 
-        '''
-        1. outer loop에서 model init, optimizer init with best model
-        (만약 처음이라면, supervised learning에서 가장 좋은 모델 가지고 옴, 아니라면 준지도 학습에서 가장 좋았던 모델 에폭 가지고 옴)
-        
-        2. with the best model, do labeling with unlabeled dataset 
-        
-        3. after per each epoch, update dataset
-        
-        4. go to the inner loop 
-        '''
-        
-        
-        ## Inner Loop 
-        '''
-        1. 추가된 학습 데이터를 통해 학습을 진행
-        
-        2. 각 epoch의 배치마다 매번 perturbed samples을 다르게 생성하도록 구현 
-        
-        3. after training all the epoch or early stopping, extract lexicon for the total labeled training dataset. 
-        
-        4. update existing lexicon based on extracted lexicon
-        '''
-        
+                    # best accuracy 혹은 low dev loss 에서의 attention 렉시콘 정보를 기반으로 사전 생성
+                    break
+                    
+                with torch.no_grad():
+                    # evaluation
+                
+                if inner_early_stopping.early_stop:
+                    break
+            
+            self.lexicon_instance.lexicon_update()
+            self.lexicon_instance.augment_lexicon()
+            1/0
+            
+            del model, optimizer
+            
