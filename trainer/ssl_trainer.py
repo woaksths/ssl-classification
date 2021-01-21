d90ff350a58ebc32062868042ef84430cfb001ec
jaeman <woaksths@gmail.com>
2021-01-21 21:59:12 +0900

    update ssl_trainer module: filter_by confidence, filter_by_lexicon, combine dataset, balance_dataset

--- trainer/ssl_trainer.py
+++ trainer/ssl_trainer.py
@@ -5,12 +5,17 @@ from util.checkpoint import save_model, get_best_checkpoint, load_trained_model,
 from transformers import BertTokenizer
 from model.bert_classification import BERT_Classification
 from model.bert_bilstm_classification import BERT_LSTM_Classification
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
+from dataset.imdb_dataset import read_imdb_split, ImdbDataset
+from dataset.dataset import Dataset
+
+
 from torch import cuda
 import torch.nn as nn 
 import copy
 import torch.nn.functional as F
 from lexicon_util.lexicon_config import *
+from lexicon_util.lexicon_operation import Lexicon
 
 
 class SSL_Trainer(object):
@@ -28,121 +33,8 @@ class SSL_Trainer(object):
         if not os.path.exists(self.ssl_expt_dir):
             os.makedirs(self.ssl_expt_dir)
         
-
-        
-    def update_dataset(self, new_labeled_dataset, labeled_dataset):
-        '''
-        @param new_labeled_dataset: type list, list의 element: tuple (dict:encodings, list:labels)
-        @param labeled_dataset: DATASET composed of encodings and labels
-        @return training_dataset = new_labeled_dataset + labeled_dataset
-        '''
-        training_dataset = copy.deepcopy(labeled_dataset)
-
-        for new_labeled_data in new_labeled_dataset:
-            input_ids = new_labeled_data[0]['input_ids']
-            attention_mask = new_labeled_data[0]['attention_mask']
-            token_type_ids = new_labeled_data[0]['token_type_ids']
-            labels = new_labeled_data[1]
-            
-            training_dataset.encodings['input_ids'].extend(input_ids)
-            training_dataset.encodings['attention_mask'].extend(attention_mask)
-            training_dataset.encodings['token_type_ids'].extend(token_type_ids)
-            training_dataset.labels.extend(labels)
-            
-        return training_dataset
-
+        self.confidence_threshold = 0.9
     
-    
-    def balance_dataset(self, new_labeled_dataset):
-        label_stats = {label: 0 for label in range(self.config.class_num)}
-        for idx, label_data in enumerate(new_labeled_dataset):
-            encodings = label_data[0]
-            labels = label_data[1]
-            for label_idx in labels:
-                label_stats[label_idx] +=1
-        min_val = 987654321
-        
-        for label, cnt in label_stats.items():
-            if min_val > cnt:
-                min_val = cnt
-                
-        dataset_size = {label: 0 for label in range(self.config.class_num)}
-        balanced_encodings = {'input_ids':[], 'attention_mask':[], 'token_type_ids':[]}
-        balanced_labels = []
-
-        for i, label_data in enumerate(new_labeled_dataset):
-            encodings = label_data[0]
-            labels = label_data[1]
-            
-            for j, label in enumerate(labels):
-                if dataset_size[label] < min_val :
-                    dataset_size[label] +=1
-                    balanced_encodings['input_ids'].append(encodings['input_ids'][j])
-                    balanced_encodings['attention_mask'].append(encodings['attention_mask'][j])
-                    balanced_encodings['token_type_ids'].append(encodings['token_type_ids'][j])
-                    balanced_labels.append(label)
-
-            if len(balanced_labels) *self.config.class_num == min_val:
-                break
-            
-        print('#INFO PSEUDO-LABEL STATS', label_stats)
-        print('#INFO NUM OF BALANCED DATASET ', len(balanced_labels))
-        return balanced_encodings, balanced_labels
-
-        
-    def pseudo_labeling(self, batch, model_preds, lexicon):
-        decode_sents = []
-        # decoding input_ids
-        for ids in batch['input_ids']:
-            sent = self.tokenizer.decode(ids)
-            sent = sent.replace('[CLS]','')
-            sent = sent.replace('[PAD]', '')
-            sent = sent.replace('[SEP]', '').strip()
-            decode_sents.append(sent)
-        
-        # lexicon based labeling
-        lexicon_preds = []
-        for sent in decode_sents:
-            matching_cnt = {label:0 for label in lexicon.keys()}
-            for word in sent.split(' '):
-                for label in lexicon.keys():
-                    if word in lexicon[label]:
-                        matching_cnt[label] += 1
-                        
-            lexicon_pred = -1
-            max_cnt = -1
-            for label, cnt in matching_cnt.items():
-                if max_cnt < cnt:
-                    max_cnt = cnt
-                    lexicon_pred = label
-            if max_cnt < 2: # 매칭 개수 늘리기
-                lexicon_pred = -1
-            lexicon_preds.append(lexicon_pred)
-        
-        labeled_encodings = {'input_ids':[], 'token_type_ids':[], 'attention_mask':[]}
-        labeled_labels = []
-        
-        # lexicon & model consensus labeling
-        total, correct = 0, 0
-        for idx, (model_pred, lexicon_pred) in enumerate(zip(model_preds, lexicon_preds)):
-            model_pred = model_pred.item()
-            pred_label = -1
-            target = batch['labels'][idx].item()
-            
-            if model_pred == lexicon_pred:
-                pred_label = lexicon_pred
-                
-                labeled_encodings['input_ids'].append(batch['input_ids'][idx].tolist())
-                labeled_encodings['token_type_ids'].append(batch['token_type_ids'][idx].tolist())
-                labeled_encodings['attention_mask'].append(batch['attention_mask'][idx].tolist())
-                labeled_labels.append(pred_label)
-                total +=1
-                
-            if target == pred_label:
-                correct +=1
-                
-        return (labeled_encodings, labeled_labels), (correct, total)
-        
     
     
     def extract_lexicon(self, attentions, input_ids, targets, lexicon):
@@ -159,7 +51,118 @@ class SSL_Trainer(object):
                 else:
                     lexicon[target][word] = 1
         return lexicon
+
     
+        
+    def filter_by_confidence(self, input_ids, confidences, outputs):
+        big_val, big_idx = torch.max(confidences.data, dim=1)
+        text_list = self.tokenizer.batch_decode(input_ids)
+        text_list = [text.replace('[CLS]','').replace('[PAD]','').replace('[SEP]','').strip() for text in text_list]
+        pseudo_labels = []
+        targets = []
+        for confidence, model_pred, text, target in zip(big_val, big_idx, text_list, outputs):
+            model_pred = model_pred.item()
+            target = target.item()
+            if confidence >= self.confidence_threshold:
+                pseudo_labels.append((text, model_pred))
+                targets.append(target)
+        return pseudo_labels, targets
+    
+    
+    
+    def filter_by_lexicon(self, pseudo_labels, targets, lexicon):
+        labeled_data = {'text':[], 'label':[]}
+        total_cnt = 0 
+        match_cnt = 0
+        
+        for (text, model_pred), target in zip(pseudo_labels, targets):
+            lexicon_cnts = {label:0 for label in lexicon.keys()}
+            for word in text.split(' '):
+                for label in lexicon.keys():
+                    if word in lexicon[label]:
+                        lexicon_cnts[label] += 1
+            lexicon_pred = -1
+            max_cnt = -1
+            is_tie = False
+            for label, cnt in lexicon_cnts.items():
+                if max_cnt == cnt:
+                    is_tie = True
+                    
+                if max_cnt < cnt:
+                    max_cnt = cnt
+                    lexicon_pred = label
+                    is_tie = False
+            
+            if model_pred == lexicon_pred and is_tie == False:
+                labeled_data['text'].append(text)
+                labeled_data['label'].append(model_pred)
+                total_cnt += 1
+                
+                if model_pred == target:
+                    match_cnt +=1
+        return labeled_data, (match_cnt, total_cnt)
+        
+    
+    def balance_dataset(self, pseudo_labeled_dataset):
+        texts = []
+        labels = []
+        for labeled_dataset in pseudo_labeled_dataset:
+            texts.extend(labeled_dataset['text'])
+            labels.extend(labeled_dataset['label'])
+        data_num_per_label = {label:0 for label in range(self.config.class_num)}
+        for label in labels:
+            data_num_per_label[label] += 1
+        
+        min_value = 987654321
+        for label, value in data_num_per_label.items():
+            if min_value > value:
+                min_value = value
+                
+        data_num_per_label = {label:0 for label in range(self.config.class_num)}
+        balanced_texts = []
+        balanced_labels = []
+        
+        if min_value == 0:
+            return None 
+        else:
+            while True:
+                if len(balanced_texts) == (self.config.class_num)*min_value:
+                    break
+
+                text = texts.pop(0)
+                label = labels.pop(0)
+
+                if data_num_per_label[label] >= min_value:
+                    texts.append(text)
+                    labels.append(label)
+                else:
+                    balanced_texts.append(text)
+                    balanced_labels.append(label)
+                    data_num_per_label[label] +=1
+                    
+            #encodings
+            text_encodings = self.tokenizer(balanced_texts, truncation=True, padding=True)
+            balanced_dataset = Dataset(text_encodings, balanced_labels)
+            return balanced_dataset
+    
+
+    
+    def combine_dataset(self, pseudo_labeled_data, labeled_data):
+        pseudo_labeled_texts = self.tokenizer.batch_decode(pseudo_labeled_data.encodings['input_ids'])
+        labeled_texts = self.tokenizer.batch_decode(labeled_data.encodings['input_ids'])
+        
+        pseudo_labeled_texts = [text.replace('[CLS]','').replace('[PAD]','').replace('[SEP]','').strip() for text in pseudo_labeled_texts]
+        labeled_texts = [text.replace('[CLS]','').replace('[PAD]','').replace('[SEP]','').strip() for text in labeled_texts]
+        
+        combined_texts = pseudo_labeled_texts + labeled_texts
+        combined_labels = pseudo_labeled_data.labels + labeled_data.labels
+        
+        combined_encodings = self.tokenizer(combined_texts, truncation=True, padding=True)
+        combined_dataset = Dataset(combined_encodings, combined_labels)
+        
+        filter_words = Lexicon.filter_words(combined_texts, combined_labels)
+        return combined_dataset, filter_words
+
     
     
     def train(self, labeled_data=None, unlabeled_data=None,
@@ -167,6 +170,7 @@ class SSL_Trainer(object):
         outer_early_stopping = EarlyStopping(patience=5, verbose=True)
         outer_lowest_dev_loss = 987654321
         outer_best_dev_accuracy = -1 
+        best_test_accuracy = -1
         
         valid_loader = DataLoader(dev_data, **self.config.valid_params)
         test_loader = DataLoader(test_data, **self.config.test_params)
@@ -180,9 +184,7 @@ class SSL_Trainer(object):
                 print('EARLY STOPPING!')
                 print('END ...')
                 break
-                
             expt_dir = self.expt_dir if o_epoch == 0 else self.ssl_expt_dir
-            
             # init model and load best model 
             checkpoint = get_best_checkpoint(expt_dir, is_best_acc=False)
             model, optimizer = load_trained_model(checkpoint, load=True)
@@ -196,7 +198,7 @@ class SSL_Trainer(object):
                 # using only model pred
                 model_total = 0
                 model_correct = 0
-                new_labeled_dataset = []
+                pseudo_labeled_dataset = []
                 
                 for _, batch in enumerate(unlabeled_loader):
                     ids = batch['input_ids'].to(self.device, dtype=torch.long)
@@ -205,38 +207,43 @@ class SSL_Trainer(object):
                     targets = batch['labels'].to(self.device, dtype=torch.long)
                     
                     outputs, attn = model(ids, attention_mask, token_type_ids)
-                    # Todo: Softmax로 confidence 값 > 0.9 반영
-                    big_val, big_idx = torch.max(outputs.data, dim=1)
+                    
+                    confidences = F.softmax(outputs, dim=-1)
+                    big_val, big_idx = torch.max(confidences.data, dim=1)
                     model_correct += (big_idx==targets).sum().item()
                     model_total += targets.size(0)
                     
-                    (labeled_encodings, labeled_labels), (correct, total) = self.pseudo_labeling(batch, big_idx, self.lexicon_instance.lexicon)
-                    new_labeled_data = (labeled_encodings, labeled_labels)
+                    pseudo_labels, targets = self.filter_by_confidence(ids, confidences, targets)
+                    pseudo_labeled_data, (correct, total) = self.filter_by_lexicon(pseudo_labels, targets, self.lexicon_instance.lexicon)
+                    pseudo_labeled_dataset.append(pseudo_labeled_data)
+
                     consensus_correct += correct
                     consensus_total += total
-                    new_labeled_dataset.append(new_labeled_data)
             
             print('#'*50, 'OUTER EPOCH {}'.format(o_epoch+1), '#'*50)
             print('INFO correct {} / total {} by only model predict'.format(model_correct, model_total))
             print('INFO consensus correct {} / consensus total {}'.format(consensus_correct, consensus_total))
+            
+            pseudo_labeled_dataset = self.balance_dataset(pseudo_labeled_dataset)
             print('UPDATE DATASET')
             
-            # balance new labeled_dataset 
-            print(type(new_labeled_dataset), type(labeled_data))
-            new_labeled_dataset = self.balance_dataset(new_labeled_dataset)
-            train_dataset = self.update_dataset([new_labeled_dataset], labeled_data)
-            print('LABELED DATA :',len(labeled_data))
-            print('TRAINING DATA [AUGMENTED]: ', len(train_dataset))
+            if pseudo_labeled_dataset is None:
+                combined_dataset = labeled_data
+                filter_words = self.lexicon_instance.filter_words
+            else:
+                # type: Dataset(encodings, labels)
+                combined_dataset, filter_words = self.combine_dataset(pseudo_labeled_dataset, labeled_data)
             
-            # Todo: Unlabeld data에 레이블링을 달 때, 사용했던 model과 optimization을 버리고, initialization (Overfitting 방지)
-            '''
+            print('TRAINING DATA [AUGMENTED]: ', len(combined_dataset))
+            
+            # Teacher initialization 
             del model, optimizer
-            model = BERT_LSTM_Classification(class_num=self.config.class_num)
+            model = BERT_LSTM_Classification(class_num=self.config.class_num, bidirectional=self.config.bidirectional)
             model.to(self.config.device)
-            optimizer = torch.optim.Adam(params = model.parameters(), lr=self.config.learning_rate)    
-            '''
+            optimizer = torch.optim.Adam(params = model.parameters(), lr=self.config.learning_rate)
+            
             # Inner Loop 
-            train_loader = DataLoader(train_dataset, **self.config.train_params)
+            train_loader = DataLoader(combined_dataset, **self.config.train_params)
             inner_early_stopping = EarlyStopping(patience=5, verbose=True)
             inner_lowest_dev_loss = 987654321 
             inner_best_dev_accuracy = -1
@@ -260,8 +267,7 @@ class SSL_Trainer(object):
                     loss1 = self.criterion(outputs, targets)
                     lexicon = self.extract_lexicon(attn, ids, targets, lexicon)
                     
-                    # Todo: gen_perturbed_batch(batch, class_label=2) 
-                    # class가 binary일 떄는 antonym. else oversampling based on synonym
+                    # gen_perturbed_batch(batch, per_sample=1, lexicon)
                     
                     loss = loss1 # + loss2 from perturbed samples
                     tr_loss += loss.item()
@@ -304,8 +310,10 @@ class SSL_Trainer(object):
                     print('EARLY STOPPING!')
                     break
             
-            lexicon = load_lexicon(self.ssl_expt_dir + '/lexicon_{}.pkl'.format(o_epoch+1))
+            lexicon = load_lexicon(self.ssl_expt_dir + '/lexicon_{}.pkl'.format(o_epoch+1))            
+            self.lexicon_instance.filter_words = filter_words
             self.lexicon_instance.lexicon_update(lexicon)
+            
             outer_early_stopping(inner_lowest_dev_loss)
             
             if inner_lowest_dev_loss < outer_lowest_dev_loss:
@@ -319,14 +327,17 @@ class SSL_Trainer(object):
                 outer_best_dev_accuracy = inner_best_dev_accuracy
                 outer_early_stopping.counter = 0
             
-            if o_epoch % 2 == 0:
+            if o_epoch % 1 == 0:
                 if test_data is not None:
                     with torch.no_grad():
                         model.eval()
                         test_loss, test_acc = self.evaluate(model, test_loader)
                         print('Test Loss ', test_loss)
                         print('Test Accuracy ', test_acc)
-                        
+                        print('best_test_accuracy', best_test_accuracy)
+                        if best_test_accuracy < test_acc:
+                            best_test_accuracy = test_acc
+                            
             del model, optimizer            
             
          
