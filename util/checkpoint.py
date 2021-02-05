fe6fa3d3caa04f574ddec5d8b40901ea2df87941
jaeman <woaksths@gmail.com>
2021-02-05 12:14:28 +0900

    modify save, load module

--- util/checkpoint.py
+++ util/checkpoint.py
@@ -7,31 +7,27 @@ from model.bert_bilstm_classification import BERT_LSTM_Classification
 import constant as config
 from transformers import AdamW, get_linear_schedule_with_warmup
 
-def save_model(model=None, optimizer=None, epoch=None, path=False, val_loss_lowest=False):
+def save_model(model=None, optimizer=None, epoch=None, path=False, is_best_checkpoint=False):
     print('SAVE MODEL')
     checkpoint = {'epoch': epoch, 'model_state_dict': model.state_dict(),
                   'optimizer_state_dict': optimizer.state_dict()}
-    if val_loss_lowest == True:
-        save_path = path +'/lowest_val_loss.pt'
+    if is_best_checkpoint == True:
+        save_path = path +'/best_checkpoint.pt'
     else:
-        save_path = path+ '/checkpoint_{}.pt'.format(epoch)
+        save_path = path +'/checkpoint_{}.pt'.format(epoch)
     torch.save(checkpoint, save_path)
+
     
-
-def get_best_checkpoint(expt_dir, is_best_acc=True):
-    if is_best_acc: # best val accuracy 
-        checkpoint = torch.load(expt_dir + '/best_checkpoint.pt')
-        return checkpoint
-    else: # lowest val loss 
-        checkpoint = torch.load(expt_dir + '/lowest_val_loss.pt')
-        return checkpoint
-
+def get_best_checkpoint(expt_dir):
+    checkpoint = torch.load(expt_dir + '/best_checkpoint.pt')
+    return checkpoint
+    
 
 def load_trained_model(checkpoint, load=True):
     # initialize
     model = BERT_LSTM_Classification(class_num=config.class_num, bidirectional=config.bidirectional)
     model.to(config.device)
-#     optimizer = torch.optim.Adam(params = model.parameters(), lr=config.learning_rate)    
+    #optimizer = torch.optim.Adam(params = model.parameters(), lr=config.learning_rate)    
     optimizer = AdamW(model.parameters(), lr=config.learning_rate, eps=1e-8)
     if load is True:
         model.load_state_dict(checkpoint['model_state_dict'])
